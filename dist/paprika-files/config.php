f3d717b5d186154c504fef5e96218d673e0eb2c2
Tomoya Koyanagi <tomk79@gmail.com>
2019-11-28 15:14:51 +0900

    update paprika develop

--- dist/paprika-files/config.php
+++ dist/paprika-files/config.php
@@ -9,19 +9,33 @@ return call_user_func( function(){
 	/** コンフィグオブジェクト */
 	$conf = new stdClass;
 
+	// ログ関連
+
+	/** ログ出力先ディレクトリ */
+	$conf->realpath_log_dir = __DIR__.'/logs/';
+
+	/** 
+	 * 出力するログレベル
+	 * ここに指定したレベル以上の情報がログファイルに出力されます。
+	 * none, fatal, error, warn, info, debug, trace, all のいずれかを指定できます。
+	 * デフォルトは all レベルです。
+	 */
+	$conf->log_reporting = 'warn';
+
+
+	// データベース接続関連
+	$conf->db = new stdClass;
+	$conf->db->connection = 'sqlite';
+	$conf->db->host = null;
+	$conf->db->port = null;
+	$conf->db->database = __DIR__.'/_database.sqlite';
+	$conf->db->username = null;
+	$conf->db->password = null;
+	$conf->db->prefix = null;
+
 	// Plugins
 	$conf->prepend = [
 		function( $paprika ){
-			$pdo = new \PDO(
-				'sqlite:'.$paprika->env()->realpath_homedir.'_database.sqlite',
-				null, null,
-				array(
-					\PDO::ATTR_PERSISTENT => false, // ←これをtrueにすると、"持続的な接続" になる
-				)
-			);
-			$paprika->add_custom_method('pdo', function() use ($pdo){
-				return $pdo;
-			});
 		},
 	];
 
